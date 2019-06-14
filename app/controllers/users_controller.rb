7ca10f33ea6bbcae4c4160aab5158b6a4754c59d
David Greenspan <ramigreenspan@gmail.com>
2019-06-14 15:41:08 -0400

    caution commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def show
+
+  end
   def new
     @user = User.new
   end
@@ -6,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     session[:user_id] = @user.id
-    redirect_to rappers_path
+    redirect_to signup_path
   end
 
 
