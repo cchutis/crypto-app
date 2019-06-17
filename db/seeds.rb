d460f48a7b158e7b4be8bd5d60befee66d196067
Constantine Chutis <constantinec84@gmail.com>
2019-06-17 10:59:30 -0400

    Added Seed Data and API

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-bitcoin = Crypto.create(value: )
+Crypto.create(name: Crypto.name(1), description: Crypto.description(1), logo: "404", symbol: Crypto.symbol(1), value: Crypto.current_value(1))
+Crypto.create(name: Crypto.name(2), description: Crypto.description(2), logo: "404", symbol: Crypto.symbol(2), value: Crypto.current_value(2))
+Crypto.create(name: Crypto.name(3), description: Crypto.description(3), logo: "404", symbol: Crypto.symbol(3), value: Crypto.current_value(3))
+Crypto.create(name: Crypto.name(4), description: Crypto.description(4), logo: "404", symbol: Crypto.symbol(4), value: Crypto.current_value(4))
+Crypto.create(name: Crypto.name(5), description: Crypto.description(5), logo: "404", symbol: Crypto.symbol(5), value: Crypto.current_value(5))
+Crypto.create(name: Crypto.name(6), description: Crypto.description(6), logo: "404", symbol: Crypto.symbol(6), value: Crypto.current_value(6))
+Crypto.create(name: Crypto.name(7), description: Crypto.description(7), logo: "404", symbol: Crypto.symbol(7), value: Crypto.current_value(7))
+Crypto.create(name: Crypto.name(8), description: Crypto.description(8), logo: "404", symbol: Crypto.symbol(8), value: Crypto.current_value(8))
+Crypto.create(name: Crypto.name(9), description: Crypto.description(9), logo: "404", symbol: Crypto.symbol(9), value: Crypto.current_value(9))
+Crypto.create(name: Crypto.name(10), description: Crypto.description(10), logo: "404", symbol: Crypto.symbol(10), value: Crypto.current_value(10))
